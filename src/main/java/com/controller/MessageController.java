d605a51bb2290afc24c2478d5798bad10ab8a166
flyyaya <895094278@qq.com>
2017-12-03 12:43:03 +0800

    12.3

--- src/main/java/com/controller/MessageController.java
+++ src/main/java/com/controller/MessageController.java
@@ -18,9 +18,9 @@ public class MessageController {
     @Autowired
     MessageService messageService;
 //摄影师获取，用户留言
-// public  String getUerMessages(Long photographerId, Model model){
-//     List<U_to_p_message> messages=messageService.getUMessages(photographerId);
-//     model.addAttribute("messages",messages);
-//     return "u-p-messages";
-// }
+ public  String getUerMessages(Long photographerId, Model model){
+     List<U_to_p_message> messages=messageService.getUMessages(photographerId);
+     model.addAttribute("messages",messages);
+     return "u-p-messages";
+ }
 }
--- src/main/java/com/service/MessageService.java
+++ src/main/java/com/service/MessageService.java
@@ -1,6 +1,7 @@
 package com.service;
 
 import com.bean.U_to_p_message;
+import com.dao.U_to_p_messageMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,12 @@ import java.util.List;
  */
 @Service
 public class MessageService {
-//    @Autowired
-//    U_to_p_messageMapper u_to_p_messageMapper;
-//    //1.查询对应Id的摄影师的留言信息
-//    public List<U_to_p_message> getUMessages(Long photographerId){
-//
-//        return  u_to_p_messageMapper.selectByPhotographerId(photographerId);
-//    }
+    @Autowired
+    U_to_p_messageMapper u_to_p_messageMapper;
+    //1.查询对应Id的摄影师的留言信息
+    public List<U_to_p_message> getUMessages(Long photographerId){
+
+        return  u_to_p_messageMapper.selectByPhotographerId(photographerId);
+    }
 
 }
